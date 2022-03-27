b72814c7f569aad733c9de5d7edb518b8586bbf8
zakariaNADAH <56134755+zakariaNADAH@users.noreply.github.com>
2022-03-27 23:33:41 +0200

    changing JPG to jpg

--- test.py
+++ test.py
@@ -24,10 +24,10 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 col = st.columns(4)
 with col[0]:
-    st.image("Logo_UT3.JPG")
+    st.image("Logo_UT3.jpg")
     
 with col[3]:
-    st.image("ensiacet.JPG")
+    st.image("ensiacet.jpg")
 
 st.markdown('---')
 
