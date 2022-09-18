dfdf6eb6b4def3203d2510fef2c7d99e53f410f4
zakariaNADAH <56134755+zakariaNADAH@users.noreply.github.com>
2022-09-18 15:02:28 +0200

    modif 4

--- dist.py
+++ dist.py
@@ -42,8 +42,36 @@ with st.sidebar:
     sidebar = option_menu(None, ['Settings'], 
         icons=['sliders'], menu_icon="cast", default_index=0)
     
-    M_ben = 78.11
-    M_tol = 92.14
+    M_ = [78.11, 92.14, 46.07, 18.01528, 60.0952]
+    
+    options = ['Benzene', 'Toluene', 'Ethanol', 'Water', 'Propanol']
+    option_1 = st.selectbox(
+    'Light Key Compound (LK)',options, index=0)
+    if option_1 == options[0]:
+        M_LK = M_[0]
+    elif option_1 == options[1]:
+        M_LK = M_[1]
+    elif option_1 == options[2]:
+        M_LK = M_[2]
+    elif option_1 == options[3]:
+        M_LK = M_[3]
+    elif option_1 == options[4]:
+        M_LK = M_[4]
+    
+    option_2 = st.selectbox(
+    'Heavy Key Compound (HK)',
+    options, index=1)
+    if option_2 == options[0]:
+        M_HK = M_[0]
+    elif option_2 == options[1]:
+        M_HK = M_[1]
+    elif option_2 == options[2]:
+        M_HK = M_[2]
+    elif option_2 == options[3]:
+        M_HK = M_[3]
+    elif option_2 == options[4]:
+        M_HK = M_[4]
+    
     alpha = st.text_input("Relative volatility (α)", 2.354)
     alpha = float(alpha)
     q = st.number_input("Fraction of liquid in the feed stream (q)", value= 1.0)
@@ -58,32 +86,34 @@ with st.sidebar:
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
     
     if Frac == 'Mole':
-        X_F = st.slider("Mole Fraction of the most volatile component in the feed (XF)",min_value=0.0, max_value=100.0, step=0.1, value=40.0)
+        X_F = st.slider("LK Mole Fraction in Feed (XF)",min_value=0.0, max_value=100.0, step=0.1, value=40.0)
         X_F = X_F/100
-        X_F_mass = X_F*M_ben/(X_F*M_ben + (1-X_F)*M_tol)
+        X_F_mass = X_F*M_LK/(X_F*M_LK + (1-X_F)*M_HK)
         
-        X_D = st.slider("Mole Fraction of the most volatile component in the distillate (XD)",min_value=0.0, max_value=100.0, step=0.1, value=99.2)
+        X_D = st.slider("LK Mole Fraction in Distillate (XD)",min_value=0.0, max_value=100.0, step=0.1, value=99.2)
         X_D = X_D/100
-        X_D_mass = X_D*M_ben/(X_D*M_ben + (1-X_D)*M_tol)
+        X_D_mass = X_D*M_LK/(X_D*M_LK + (1-X_D)*M_HK)
         
-        X_W = st.slider("Mole Fraction of the most volatile component in the bottom (XB)",min_value=0.0, max_value=100.0, step=0.1, value=01.4)
+        X_W = st.slider("LK Mole Fraction in Bottoms (XB)",min_value=0.0, max_value=100.0, step=0.1, value=01.4)
         X_W = X_W/100
-        X_W_mass = X_W*M_ben/(X_W*M_ben + (1-X_W)*M_tol)
+        X_W_mass = X_W*M_LK/(X_W*M_LK + (1-X_W)*M_HK)
         
     else:
-        X_F_mass = st.slider("Mass Fraction of the most volatile component in the feed (XF)",min_value=0.0, max_value=100.0, step=0.1, value=36.108542899408286)
+        X_F_mass = st.slider("LK Mass Fraction in Feed (XF)",min_value=0.0, max_value=100.0, step=0.1, value=36.108542899408286)
         X_F_mass = X_F_mass/100
-        X_F = X_F_mass/M_ben/(X_F_mass/M_ben + (1-X_F_mass)/M_tol)
+        X_F = X_F_mass/M_LK/(X_F_mass/M_LK + (1-X_F_mass)/M_HK)
         
-        X_D_mass = st.slider("Mass Fraction of the most volatile component in the distillate (XD)",min_value=0.0, max_value=100.0, step=0.1, value=99.05765930507743)
+        X_D_mass = st.slider("LK Mass Fraction in Distillate (XD)",min_value=0.0, max_value=100.0, step=0.1, value=99.05765930507743)
         X_D_mass = X_D_mass/100
-        X_D = X_D_mass/M_ben/(X_D_mass/M_ben + (1-X_D_mass)/M_tol)
+        X_D = X_D_mass/M_LK/(X_D_mass/M_LK + (1-X_D_mass)/M_HK)
         
-        X_W_mass = st.slider("Mass Fraction of the most volatile component in the bottom (XB)",min_value=0.0, max_value=100.0, step=0.1, value=01.1893598226216556)
+        X_W_mass = st.slider("LK Mass Fraction in Bottoms (XB)",min_value=0.0, max_value=100.0, step=0.1, value=01.1893598226216556)
         X_W_mass = X_W_mass/100
-        X_W = X_W_mass/M_ben/(X_W_mass/M_ben + (1-X_W_mass)/M_tol)
+        X_W = X_W_mass/M_LK/(X_W_mass/M_LK + (1-X_W_mass)/M_HK)
         
-    F_mass = X_F*F*M_ben + (1-X_F)*F*M_tol
+    F_mass = X_F*F*M_LK + (1-X_F)*F*M_HK
+    
+    st.write("----")
 
 # if sidebar == 'Settings':
         
@@ -162,8 +192,8 @@ if selected == "Home":
 # =============================================================================
 elif selected == "Design & Modeling":
     HTML_BANNER = """
-        <h1 style="color:#DD985C;text-align:center;">Distillation Column Disgn</h1>
-        <p style="color:#DD985C;text-align:center;">Shortcut Distillation and MacCabe-Thiele Method</p>
+        <h1 style="color:#FF4B4B;text-align:center;">Distillation Column Disgn</h1>
+        <p style="color:#FF4B4B;text-align:center;">Shortcut Distillation and MacCabe-Thiele Method</p>
         </div>
         """
     stc.html(HTML_BANNER)
@@ -175,7 +205,7 @@ elif selected == "Design & Modeling":
     st.markdown("- The actual number of trays N (or equilibrium number of trays)")
     st.markdown("- The optimum feed tray location")
     
-    st.markdown("To determine all these factors, we will use two methods: **Shortcut Distillation Method**$$~^1$$ and **The MacCabe-Thiele Method**$$~^2$$.")
+    st.markdown("To determine all these factors, we will use two methods: **Shortcut Distillation Method**$$^*$$ and **The MacCabe-Thiele Method**.")
     
     # =============================================================================
     # Définir la fonction d'équilibre
@@ -227,6 +257,7 @@ elif selected == "Design & Modeling":
     st.write(r'### <p style="text-align: center;">$$ N_{min} =\frac {ln[(X_D/(1-X_D))((1-X_B)/X_B)]}{log(\alpha)} \hspace*{0.4cm}(1)$$</p>', unsafe_allow_html=True)
     
     N_min = mt.log((X_D/(1-X_D))*((1-X_W)/X_W))/mt.log(alpha)
+    STG = N_min
     Stages_min_cal = int(N_min)+1
     
     col_N_min = st.columns(3)
@@ -516,6 +547,7 @@ elif selected == "Design & Modeling":
     
     N = fsolve(equi_sta, 0)
     N_cal = N[0]
+    STG_cal = N_cal
     
     col_NN = st.columns(3)
     with col_NN[1]:
@@ -687,7 +719,7 @@ elif selected == "Design & Modeling":
     
     
     st.subheader("4. Optimum feed tray location")
-    st.markdown("Using Kirkbride method:")
+    st.markdown("Using Kirkbride method$$~^1~$$:")
     st.write(r'### <p style="text-align: center;">$$\ln(\frac {N_{D}}{N_B})=0.206 \ln \bigg( \Big(\frac {1 - X_{F}}{X_F}\Big) \Big(\frac {X_B}{1 - X_D}\Big)^{2} \Big(\frac {B}{D}\Big) \bigg) \hspace*{0.4cm}(8)$$</p>', unsafe_allow_html=True)
         
     def materiel_balance():
@@ -699,12 +731,13 @@ elif selected == "Design & Modeling":
         return N_D, rap, D, B
     
     N_F = int(materiel_balance()[0])+1
+    STG_F = materiel_balance()[0]
     rap = materiel_balance()[1]
     D = materiel_balance()[2]
     B = materiel_balance()[3]
     
-    D_mass = X_D*D*M_ben + (1-X_D)*D*M_tol
-    B_mass = X_W*B*M_ben + (1-X_W)*B*M_tol
+    D_mass = X_D*D*M_LK + (1-X_D)*D*M_HK
+    B_mass = X_W*B*M_LK + (1-X_W)*B*M_HK
     
     col_F = st.columns(3)
     with col_F[1]:
@@ -733,9 +766,9 @@ elif selected == "Design & Modeling":
     df = pd.DataFrame(
         np.array([[ str("Reflux Ratio"),  round(R, 2),  round(R, 1)],
                   [ str("Minimum Reflux Ratio"),  round(R_min_cal, 2),  round(R_min, 2)],
-                  [str("Minimum Reflux Ratio"),  N_min_cal,  N_min],
-                  [str("Number of Trays"), round(N_cal, 2),  round(N, 2)],
-                  [str("Feed tray"), round(N_F, 2),  round(s_f[0], 2)]]),
+                  [str("Minimum Reflux Ratio"),  round(STG,2),  N_min],
+                  [str("Number of Trays"), round(STG_cal, 2),  round(N, 2)],
+                  [str("Optimal Feed Stage"), round(STG_F, 2),  round(s_f[0], 2)]]),
         columns=('','Shortcut Distillation','MacCabe-Thiele'))
     
     # df = pd.DataFrame(
@@ -758,10 +791,10 @@ elif selected == "Design & Modeling":
     df = pd.DataFrame(
         np.array([[ str("Molar Flow"),  str("Kgmole/h") , round(F, 3), round(D, 3), round(B, 3)],
                   [ str("Mass Flow"),  str("Kg/h") , round(F_mass, 3), round(D_mass, 3), round(B_mass, 3)],
-                  [str("Mole Frac (Benzene)"),  str("-") , round(X_F, 3), round(X_D, 3), round(X_W, 3)],
-                  [str("Mole Frac (Toluene)"),  str("-") , round(1-X_F, 3), round(1-X_D, 3), round(1-X_W, 3)],
-                  [str("Mass Frac (Benzene)"),  str("-") , round(X_F_mass, 3), round(X_D_mass, 3), round(X_W_mass, 3)],
-                  [str("Mass Frac (Toluene)"),  str("-") , round(1-X_F_mass, 3), round(1-X_D_mass, 3), round(1-X_W_mass, 3)]]),
+                  [str("Mole Frac (LK)"),  str("-") , round(X_F, 3), round(X_D, 3), round(X_W, 3)],
+                  [str("Mole Frac (HK)"),  str("-") , round(1-X_F, 3), round(1-X_D, 3), round(1-X_W, 3)],
+                  [str("Mass Frac (LK)"),  str("-") , round(X_F_mass, 3), round(X_D_mass, 3), round(X_W_mass, 3)],
+                  [str("Mass Frac (HK)"),  str("-") , round(1-X_F_mass, 3), round(1-X_D_mass, 3), round(1-X_W_mass, 3)]]),
         columns=('','Unite','Feed','Distillate','Bottoms'))
     
     hide_table_row_index = """
@@ -773,8 +806,8 @@ elif selected == "Design & Modeling":
     st.markdown(hide_table_row_index, unsafe_allow_html=True)
     st.table(df)
     
-    st.write("$$~^1~$$: Computer Methods in Chemical Engineering Second Edition")
-    st.write("$$~^2~$$: Materiel balnce")
+    st.write("$$~^*~$$: Computer Methods in Chemical Engineering Second Edition")
+    # st.write("$$~^2~$$: Materiel balnce")
      
         
 
@@ -795,7 +828,7 @@ elif selected == "Simulation":
         """
     stc.html(HTML_BANNER)
     
-    st.write(" Using Hysys and DWSIM shortcut distillation method, select Peng-Robinson (PR) as the fluid package, connect feed and product streams, distillate and product streams, condenser and reboiler energy streams, and fully specifying feed stream. While on Design/Parameters page, the following data should be made available:")
+    st.write(" Using Hysys or DWSIM shortcut distillation method, select Peng-Robinson (PR) as the fluid package, connect feed and product streams, distillate and product streams, condenser and reboiler energy streams, and fully specifying feed stream. While on Design/Parameters page, the following data should be made available:")
     st.write("* LK in the bottom: Benzene with mole fraction 0.014")
     st.write("* HK in distillate: Toluene, mole fraction 0.008")
     st.write("* Condenser and reboiler pressure: 1 atm")
